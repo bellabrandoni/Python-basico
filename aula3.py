9d871e19ba33775e596a2a802263fdc60bd299d1
unknown <isabella14brandao@gmail.com>
2024-02-15 16:08:03 -0300

    Tipos de dados em Python primeira parte- string

---
+++ aula3.py
@@ -0,0 +1,19 @@
+"""
+Python  - tipagem = dinamica/forte
+dinamica - detecta o tipo de dado Tipagem dinâmica significa que o próprio interpretador do Python infere o tipo dos dados que uma variável recebe, sem a necessidade que você, o usuário da linguagem diga de que tipo determinada variável é.
+
+STRING EM PYTHON = str
+Strings - tipo de texto
+str->string ->texto
+"""
+#Escape -\ ->caractere de escape
+print('Isabella \'Brandao')
+
+#escape é diferente da virgula que separa dois valores
+print('isabella','brandao')
+
+# r - outro jeito de escape
+print(r'Isabella\'Brandao\r')
+
+#codigo limpo - fazendo com que as aspas aparecam ->isabella "brandao"
+print('isabella "brandao"')
