6aa0887f8d6fc59afebb6a408518a296bae2149f
unknown <isabella14brandao@gmail.com>
2024-02-15 15:46:19 -0300

    funcao print em py

---
+++ aula2.py
@@ -0,0 +1,8 @@
+#print-funcao de exibicao em python
+print(12,34) #print-funcao de exibicao em python
+
+#sep ='' - separador - searou 12 - 34
+print(12,34, sep="-")
+
+#end - aparece no final do print -> 12-34;
+print(12,34, sep="-", end=';')
