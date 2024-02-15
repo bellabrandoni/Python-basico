56edd1319a34fbea7b0a604292bfde8e0e5c186a
unknown <isabella14brandao@gmail.com>
2024-02-15 15:32:53 -0300

    tipos de cometario em py -aspas simples

--- aula1.py
+++ aula1.py
@@ -3,7 +3,11 @@
 print(123) #comentando na frente da linha
 #cometario abaixo da linha
 
-"""outra 
-    forma
-    de comentario em python
+"""outra  forma de comentario em python
+    Este tipo de comentario é chamado de DocString
+    Neste caso, o interpretador do python le este comentario
 """
+
+'''Criando codigo com aspas 
+    simples
+'''
