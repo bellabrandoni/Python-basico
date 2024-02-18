95a2488a065d968604e28ce415b65b2f16923504
unknown <isabella14brandao@gmail.com>
2024-02-18 12:33:32 -0300

    variaveis em python

---
+++ variaveis.py
@@ -0,0 +1,28 @@
+#Variáveis são usadas para salvar algo na memória do computador, usadas para tornar o codigo mais legível e para não repetir o código em lugares que vai usar a mesma coisa
+#PEP8: inicie variáveis com letras minúsculas, pode usar numeros e underline_
+#O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a uma variável
+#Uso -> nome_variavel = expressão
+
+nome_completo = 'Luiz'
+soma_dois_mais_dois = 2+2
+
+print(nome_completo, soma_dois_mais_dois)# -> Luiz, 4 
+
+#outro exemplo
+nome ='Isabella'
+idade = 24
+maior_de_idade = idade >=18
+print('Nome:',nome, 'Idade:', idade)
+print('É maior?', maior_de_idade) #-> true
+
+    
+    
+#Conversao de tipo de dado
+#antes 
+print(int('1'))
+
+#utilizando variaveis
+int_um = int('1')
+print(int_um) #->1
+
+
