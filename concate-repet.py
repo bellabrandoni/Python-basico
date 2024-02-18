4b5d3d4bfefeebe2cc861423c9fbe0729f2c7a0c
unknown <isabella14brandao@gmail.com>
2024-02-18 14:31:02 -0300

    Concatenacao de dados e repeticao

---
+++ concate-repet.py
@@ -0,0 +1,18 @@
+
+#Concatenção - uniao de valores - DEVEM SER DO MESMO TIPO 
+concatenacao = 'A'+'B'+'C'
+print(concatenacao)
+
+#concatenacao com tipos de dados diferentes  
+conc = 'luiz'+ 1 + 'otavio' #->error
+
+#concatencao com tipos de dados diferentes - mudando o dado para string
+conc_tipos_dif_dados = 'Luiz' + str(1)+''+ 'otavio'
+print(conc_tipos_dif_dados)
+
+
+#Repeticao
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'Luiz'
+print (a_dez_vezes)
+print (tres_vezes_luiz)
