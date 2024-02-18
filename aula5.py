dd5153dd3b6b49eb4d83b1a29df81761356d0589
unknown <isabella14brandao@gmail.com>
2024-02-18 11:58:56 -0300

    Tipo de dados -bool

---
+++ aula5.py
@@ -0,0 +1,9 @@
+#Tipo de dado bool (Boolean)
+#Ao questionar algo em um programa, so existem duas respostas possiveis:
+#sim(true) ou nao(false).
+#Existem vários operadoes para "questionar".
+#Dentre eles o == que é um operador lógico que confere se um valor é igual a outro valor e se eles são do mesmo tipo.  
+
+print(10 == 10) #Sim ->True
+print(10 == 11) #Nao -> False
+print(type(10 == 10)) #->bool = retorna o tipo de dado booleano 
