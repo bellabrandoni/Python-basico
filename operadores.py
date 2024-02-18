22a10fc510259885659cbba7039f983851b5bc90
unknown <isabella14brandao@gmail.com>
2024-02-18 13:07:40 -0300

    operadores logicos em python

---
+++ operadores.py
@@ -0,0 +1,21 @@
+adicao = 10 + 10
+print('Adicao:', adicao)
+
+subtracao = 10-5
+print('Subtracao:', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicacao:',multiplicacao)
+
+divisao = 10 / 2.2 #-> sempre vai retornar um numnero de ponto flutuante
+print('Divisao:', divisao)
+
+divisao_inteira = 10 // 2.2 #-> se passar um numero de ponto flutuante ele retorna ua divisao zerada (4.0), este tipo de divisao nao retorna casas decimais
+print('Divisao inteira:', divisao_inteira)
+
+exponencial = 2 ** 10 #->potenciacao
+print('Exponencial:', exponencial)
+
+modulo = 55 % 2 #retorna o resto da divisao
+print('Modulo:', modulo)
+
