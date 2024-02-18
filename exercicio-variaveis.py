957bf5bb153ba739f28d4a9264344e2a33811d31
unknown <isabella14brandao@gmail.com>
2024-02-18 12:47:20 -0300

    exercicio-variaveis-em-python

---
+++ exercicio-variaveis.py
@@ -0,0 +1,19 @@
+
+#Crie variaveis para cada tipo de dado do print
+nome = 'Isabella'
+print('Nome:', nome)
+
+sobrenome = 'Brandao'
+print('Sobrenome:', sobrenome)
+
+idade = 24
+print('Idade:', idade)
+
+ano_de_nascimento = 2024 - idade
+print('Ano de nascimento:', ano_de_nascimento)
+
+maior_de_idade = idade >= 18
+print('Maior de idade:', maior_de_idade)
+
+altura_metros = 1.80
+print('Altura em metros:', altura_metros)
