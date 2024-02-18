0fc50ba53359ea1d1389b78db7b7117086bcc052
unknown <isabella14brandao@gmail.com>
2024-02-18 12:13:17 -0300

    conversao de dados/type-convertion/typecasting

---
+++ aula6.py
@@ -0,0 +1,18 @@
+#conversao de tipos, coerção 
+#type convertion, typecasting, coersion
+#é o ato de converter um tipo em outro tipo imutáveis e primitivos:
+#str, int, florar, bool, -imutáveis
+
+print(1+1)
+print('a' + 'b')
+
+#funcoes de conversao
+#classe int - converte o tipo texto em inteiro 
+#type - mostra a conversao do tipo de dado
+print(int('1'), type(int('1')))
+print(int('1') + 1)
+
+
+#convertendo bool
+
+print(bool(''))
